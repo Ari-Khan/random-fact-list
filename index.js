3ee1ee7719dc55ff7c9b933fe6bf009ce320d13a
Ari <ari.basil.khan@gmail.com>
2024-12-11 11:29:06 -0500

    Edited index.js.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { randomFactList } from "./list";
+import { randomFactList } from "./list.js";
 
 export default function getRandomFact() {
     const random = Math.floor(Math.random() * randomFactList.length);
